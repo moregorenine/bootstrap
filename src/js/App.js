8535797448e115bd4bbf4250817bd6abd014f599
moregorenine <moregorenine@gmail.com>
2021-11-07 11:20:00 +0900

    [feature] add package.json sass

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+import '../scss/App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>learn react</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
